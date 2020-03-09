bcc5e7806c9d77bf3f1162e1f7c600265d697a31
Andrei Chapiuk <venik2281488@gmail.ru>
2020-03-09 18:30:29 +0300

    complite:complite 'towel-sort'-task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
+  const arr = matrix;
+  let answer = [];
+if ( arr === undefined) {//для ошибок
   return [];
 }
+  if ( arr.length == 0) {//для ошибок
+  return [];
+}
+  if (arr.length > 1) {// проверка не пустой ли массив приходит
+for (let index = 0; index < arr.length; index++) {
+  let arrLang = arr[index].length;
+for (let i = 0; i < arrLang; i++) {//пушим в новый массив инфу с первого приходящего
+  if( index % 2 == 0 ) {
+answer.push(arr[index][i])
+}
+let lem= arrLang-(i+1);
+  if( index % 2 !== 0 ) {// реверсим второй приходящий массив и пушим инву (начиная с  конца массива)
+answer.push(arr[index][lem])
+}
+}
+}
+
+} 
+return answer;
+}
+//входим в первый массив извлекаем данные второй массив реверсим извлекаем данные
+//входим в третий извлекаем, четверный реверсим
